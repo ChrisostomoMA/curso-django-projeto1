e6aa92fef5cc1babf778613097c6536fb9725f8e
Marcio Ansel Chrisostomo <chrisostomo.marcio@outlook.com>
2022-04-16 10:41:35 -0300

    Removido Contato e Sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,16 @@
+from django import shortcuts
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME 2')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Marcio Chrisostomo',
+    })
 
 
 def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'me-apague/temp.html')
 
 
 def sobre(request):
